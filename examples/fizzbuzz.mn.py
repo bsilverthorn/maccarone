de4a47696a75ec06a9c77a758ecf9f87c9eb19a6
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-05-19 17:47:35 -0700

    examples: add fizzbuzz, todo.

---
+++ examples/fizzbuzz.mn.py
@@ -0,0 +1,7 @@
+def main(n: int):
+    #<<do fizzbuzz from 1 to n>>
+
+#<<
+# parse command line args for main
+# call main with those args
+#>>
---
+++ examples/todo.mn.py
@@ -0,0 +1,19 @@
+class Todo:
+    #<<description, status (done, not done), id number>>
+
+def load_todos() -> list[Todo]:
+    #<<load from ~/.todos.json>>
+
+def save_todos(todos: list[Todo]) -> None:
+    #<<save todos>>
+
+def add_todo(description):
+    #<<add todo and save>>
+
+def remove_todo(id):
+    #<<remove todo and save>>
+
+def print_todos():
+    #<<print todos>>
+
+#<<command line interface using argparse>>
