17324bc950c7a644fad43e8bfaefce2c4c5a589c
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-05-29 11:19:17 -0700

    maccarone/__init__: don't require stale_maccarone.
    Hack to avoid requiring stale maccarone as a runtime dependency. There
    is probably a better way to do this that we can implement later.

--- src/maccarone/__init__.py
+++ src/maccarone/__init__.py
@@ -1,4 +1,7 @@
-import stale_maccarone.enable
+try:
+    import stale_maccarone.enable
 
-stale_maccarone.enable.enable_py_string_matching = False
-stale_maccarone.enable.fullname_pattern = r"maccarone\..*"
+    stale_maccarone.enable.enable_py_string_matching = False
+    stale_maccarone.enable.fullname_pattern = r"maccarone\..*"
+except ImportError:
+    pass
