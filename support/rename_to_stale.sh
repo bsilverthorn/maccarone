44009d4b827b31d1ad4857850ae04126d663c63b
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-05-28 17:48:32 -0700

    rename_to_stale: adjust local version suffix.
    ... so we get v0.0.x.dev0 rather than something longer and even more
    painful.
    
    It's possible to remove the dev0 as well but I don't think it is harmful
    and might even be helpful in this case, because it makes it even more
    clear that there is something weird about `stale_maccarone`.

--- support/rename_to_stale.sh
+++ support/rename_to_stale.sh
@@ -1,8 +1,11 @@
 #!/bin/env bash
 
+# hackishly rewrite the project to use a different name (to support self-hosting)
+
 set -eux
 
 find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/import maccarone/import stale_maccarone/'
 find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/from maccarone/from stale_maccarone/'
 
 sed -i 's/name = "maccarone"/name = "stale_maccarone"/' pyproject.toml
+sed -i 's/local_scheme = "node-and-date"/local_scheme = "no-local-version"/' pyproject.toml
