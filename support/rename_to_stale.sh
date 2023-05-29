d688ceb2681a29c22013e235921f49ef8ea07ab3
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-05-29 11:33:22 -0700

    rename_to_stale.sh: rename maccarone script.

--- support/rename_to_stale.sh
+++ support/rename_to_stale.sh
@@ -8,5 +8,6 @@ find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/import maccarone/
 find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/from maccarone/from stale_maccarone/'
 
 sed -i 's/name = "maccarone"/name = "stale_maccarone"/' pyproject.toml
+sed -i 's/maccarone = "maccarone.scripts/stale_maccarone = "stale_maccarone.scripts/' pyproject.toml
 
 mv src/{maccarone,stale_maccarone}
