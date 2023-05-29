e07c35e2fab554e16a859189e5ee1b7a4210250f
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-05-28 19:54:51 -0700

    Use `SETUPTOOLS_SCM_PRETEND_VERSION` for stale.
    This approach is simpler to implement and does exactly what we want,
    i.e., build `stale_maccarone` with the same version number as
    `maccarone`.

--- support/rename_to_stale.sh
+++ support/rename_to_stale.sh
@@ -8,6 +8,5 @@ find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/import maccarone/
 find ./src/maccarone -name "*.py" -print0 | xargs -0 sed -i 's/from maccarone/from stale_maccarone/'
 
 sed -i 's/name = "maccarone"/name = "stale_maccarone"/' pyproject.toml
-sed -i 's/local_scheme = "node-and-date"/local_scheme = "no-local-version"/' pyproject.toml
 
 mv src/{maccarone,stale_maccarone}
