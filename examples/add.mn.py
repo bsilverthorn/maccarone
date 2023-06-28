6f5c057596204cd2b0bda3fd9b6de0415ea8e549
Bryan Silverthorn <bsilverthorn@gmail.com>
2023-06-28 11:45:52 -0700

    examples: use rewrite mode, and tweak.

--- examples/add.mn.py
+++ examples/add.mn.py
@@ -1,4 +1,13 @@
 def add_two_numbers(x, y):
-    #<<add the args>>
+    #<<add both args>>
+    return x + y
+    #<</>>
 
 #<<argparse stuff>>
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int)
+parser.add_argument("y", type=int)
+args = parser.parse_args()
+return add_two_numbers(args.x, args.y)
+#<</>>
--- examples/file_sizes_ext.mn.py
+++ examples/file_sizes_ext.mn.py
@@ -1,8 +1,30 @@
-def main(path: str, extension: str | None = None):
+#<<imports>>
+import os
+import sys
+from typing import List
+#<</>>
+
+def main(path: str, extension: str | None):
     #<<filenames = list of filenames under path; no dirs>>
+    filenames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and (extension is None or f.endswith(extension))]
+    #<</>>
 
     for fn in filenames:
         #<<size = size of fn in bytes>>
-        print(fn, size)
+        size = os.path.getsize(os.path.join(path, fn))
+        #<</>>
 
-#<<use argparse and call main>>
+        #<<print fn and size with colors>>
+        print(f"\033[1;34m{fn}\033[0m: \033[1;32m{size} bytes\033[0m")
+        #<</>>
+
+#<<use argparse and then call main>>
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Path to the directory containing files")
+parser.add_argument("-e", "--extension", help="Filter files by extension", default=None)
+args = parser.parse_args()
+
+main(args.path, args.extension)
+#<</>>
